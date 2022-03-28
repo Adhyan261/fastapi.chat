779de6668c6afaa8f809c7b5d04680c1630cff0e
Adhyan261 <sgupta.sg224@gmail.com>
2022-03-28 16:47:55 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
-from pickle import NONE
-from turtle import title
-import fastapi
-fastapi.use('Agg')
 from fastapi import FastAPI
-FastAPI.use('Agg')
-from random import randrange
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
-from .routers import user,post,auth,vote
+from .routers import post, user, auth, vote
 from .config import settings
-from fastapi.middleware.cors import CORSMiddleware
-CORSMiddleware.use('Agg')
-
 
 models.Base.metadata.create_all(bind=engine)
 
